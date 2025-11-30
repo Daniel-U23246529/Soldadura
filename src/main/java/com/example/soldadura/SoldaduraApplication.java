e8a8cbcf15adcdf7536b5cd418559575cdb1c765
cpomibnados <u23246529@utp.edup.pe>
2025-11-30 00:13:06 -0500

    primer commit

---
+++ src/main/java/com/example/soldadura/SoldaduraApplication.java
@@ -0,0 +1,13 @@
+package com.example.soldadura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoldaduraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoldaduraApplication.class, args);
+	}
+
+}
